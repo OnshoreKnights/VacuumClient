4d6a8f684106dcafd4cc8ccf9aa5c7d8a1661769
Kavrae <croberts.business@gmail.com>
2017-11-02 07:20:55 -0500

    Randomized Cleaning

--- Agent/Agent.cs
+++ Agent/Agent.cs
@@ -12,8 +12,32 @@ namespace IntelligentVacuum.Agent
 
         public AgentAction DecideAction(Room room)
         {
-            Console.WriteLine("This is a git/VS test");
-            return AgentAction.NONE;
+            AgentAction nextAction;
+            if(room.IsDirty) 
+            {
+                nextAction = AgentAction.CLEAN;
+            }
+            else 
+            {
+                Random random = new Random();
+                int nextDirection = random.Next(0,3);
+                switch(nextDirection)
+                {
+                    case 0:
+                        nextAction = AgentAction.MOVE_RIGHT;
+                        break;
+                    case 1:
+                        nextAction = AgentAction.MOVE_DOWN;
+                        break;
+                    case 2:
+                        nextAction = AgentAction.MOVE_LEFT;
+                        break;
+                    default:
+                        nextAction = AgentAction.MOVE_UP;
+                        break;
+                }       
+            }
+            return nextAction;
         }
     }
 }
